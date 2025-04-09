9a501791eba7fc41bd9b517b93a8c22cdefcf242
Shiva <Shiva@DESKTOP-LDR4V89.bbrouter>
2025-04-09 19:30:40 +0530

    First Commit MongoDb

---
+++ Boot-WebFlux-Proj-01-Reactive-Provid/src/main/java/com/kiss/repository/IPhoneRepository.java
@@ -0,0 +1,13 @@
+package com.kiss.repository;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+import com.kiss.model.Phone;
+
+import reactor.core.publisher.Flux;
+
+
+public interface IPhoneRepository extends ReactiveMongoRepository<Phone, Integer> {
+
+	public Flux<Phone> findByName(String name);
+}
